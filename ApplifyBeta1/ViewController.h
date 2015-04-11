a75558cb26108901fb52480a34102a84006ed18f
Sevy11 <michaelsevy@gmail.com>
2015-04-11 14:41:01 -0500

    Initial Commit

---
+++ ApplifyBeta1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplifyBeta1
+//
+//  Created by Michael Sevy on 4/11/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
